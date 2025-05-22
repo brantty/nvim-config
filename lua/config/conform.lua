d7b78ca9e87c950793139324d743cb12b9e3bca7
Derek <su.derek@gmail.com>
2025-05-22 09:16:14 +0800

    add zigfmt for zig filetype

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -1,18 +1,31 @@
 local opts = {
+  -- Map of filetype to formatters
   formatters_by_ft = {
     lua = { "stylua" },
-    -- conform will run multiple formatters sequentially
-    python = { "isort", "black" },
-    -- You can customize some the format options for the filetype (:help conform.format)
+    -- Conform will run multiple formatters sequentially
+    go = { "goimports", "gofmt" },
+    -- You can also customize some of the format options for the filetype
     rust = { "rustfmt", lsp_format = "fallback" },
-    -- conform will run the first available formatter
-    javascript = { "prettierd", "prettier", stop_after_first = true },
+    -- You can use a function here to determine the formatters dynamically
+    python = function(bufnr)
+      if require("conform").get_formatter_info("ruff_format", bufnr).available then
+        return { "ruff_format" }
+      else
+        return { "isort", "black" }
+      end
+    end,
+    zig = { "zigfmt" },
+    -- Use the "*" filetype to run formatters on all filetypes.
+    ["*"] = { "codespell" },
+    -- Use the "_" filetype to run formatters on filetypes that don't
+    -- have other formatters configured.
+    ["_"] = { "trim_whitespace" },
   },
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_format = "fallback",
-  }
+  },
 }
 
 require("conform").setup(opts)
